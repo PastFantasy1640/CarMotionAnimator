7b80fb2c3607f1b1e3d510cdeee807f4690d1c2e
wgprojects2013 <pastfantasy1640@gmail.com>
2017-12-16 04:16:22 +0900

    最初のファイルと、ヘッダファイルテンプレートを作成しました

---
+++ CarMotionAnimator/src/CarMotionAnimator.hpp
@@ -0,0 +1,51 @@
+/////////////////////////////////////////////////////
+//[Module name]
+//
+//Copyright (c) 2017 Shotaro Shirao
+//
+//This software is released under the MIT License.
+//http://opensource.org/licenses/mit-license.php
+/////////////////////////////////////////////////////
+
+#pragma once
+#ifndef CMA__HPP_
+#define CMA__HPP_
+
+namespace cma {
+
+/** ___replace___ CLASS
+ * ê‡ñæ
+ */
+class ___replace___{
+public:
+	//////////////////////////////
+	// PUBLIC MEMBER CONSTANT
+	//////////////////////////////
+
+
+	//////////////////////////////
+	// PUBLIC MEMBER VALIABLES
+	//////////////////////////////
+	
+	
+	//////////////////////////////
+	// PUBLIC MEMBER FUNCTION
+	//////////////////////////////
+
+	/** Constructor
+	 */	
+	___replace___();
+
+	/** Destructor
+	 */
+	~___replace___();
+
+protected:
+
+private:
+
+};
+
+};
+
+#endif //end of include guard
