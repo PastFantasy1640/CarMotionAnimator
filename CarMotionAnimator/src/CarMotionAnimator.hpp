bdcb8fcccfcdab80dc81662f5ec1b2f26194ff03
wgprojects2013 <pastfantasy1640@gmail.com>
2017-12-16 04:29:57 +0900

    トップヘッダを少し整理

--- CarMotionAnimator/src/CarMotionAnimator.hpp
+++ CarMotionAnimator/src/CarMotionAnimator.hpp
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////
-//[Module name]
+//CarMotionAnimator.hpp
 //
 //Copyright (c) 2017 Shotaro Shirao
 //
@@ -7,45 +7,26 @@
 //http://opensource.org/licenses/mit-license.php
 /////////////////////////////////////////////////////
 
+//ä¬ã´ç\ízéûÇ…éQçlÇ…ÇµÇΩURL
+//ÅyMaya C++ÅzMaya C++ÇÃénÇﬂï˚(maya2017 update4)
+//https://unpyside.wixsite.com/unpyside/single-post/2017/07/03/%E3%80%90Maya-C%E3%80%91Maya-C%E3%81%AE%E5%A7%8B%E3%82%81%E6%96%B9maya2017-update4
+
+
 #pragma once
-#ifndef CMA__HPP_
-#define CMA__HPP_
+#ifndef CMA_CARMOTIONANIMATOR_HPP_
+#define CMA_CARMOTIONANIMATOR_HPP_
+
+#include <maya/MString.h>
+#include <maya/MArgList.h>
+#include <maya/MFnPlugin.h>
+#include <maya/MPxCommand.h>
+#include <maya/MObject.h>
 
 namespace cma {
 
-/** ___replace___ CLASS
- * ê‡ñæ
- */
-class ___replace___{
-public:
-	//////////////////////////////
-	// PUBLIC MEMBER CONSTANT
-	//////////////////////////////
 
-
-	//////////////////////////////
-	// PUBLIC MEMBER VALIABLES
-	//////////////////////////////
-	
-	
-	//////////////////////////////
-	// PUBLIC MEMBER FUNCTION
-	//////////////////////////////
-
-	/** Constructor
-	 */	
-	___replace___();
-
-	/** Destructor
-	 */
-	~___replace___();
-
-protected:
-
-private:
 
 };
 
-};
 
 #endif //end of include guard
