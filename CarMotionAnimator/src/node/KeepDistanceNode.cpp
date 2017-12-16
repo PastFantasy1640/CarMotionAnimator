040e1d51fe43940ee5cedde423ad13df06285d1e
wgprojects2013 <pastfantasy1640@gmail.com>
2017-12-16 22:43:15 +0900

    テスト用チュートリアルコードを実装

--- CarMotionAnimator/src/node/KeepDistanceNode.cpp
+++ CarMotionAnimator/src/node/KeepDistanceNode.cpp
@@ -1,4 +1,5 @@
 #include "KeepDistanceNode.hpp"
+#include <cmath>
 
 const MTypeId cma::KeepDistanceNode::id(cma::KeepDistanceNode::kKeepDistanceNodeID);
 
@@ -7,13 +8,38 @@ cma::KeepDistanceNode::KeepDistanceNode() {}
 cma::KeepDistanceNode::~KeepDistanceNode() {}
 
 MStatus cma::KeepDistanceNode::compute(const MPlug & plug, MDataBlock & data) {
+	MStatus returnStatus;
+	if (plug == output) {
+		MDataHandle inputData = data.inputValue(input, &returnStatus);
+		if (returnStatus != MS::kSuccess) cerr << "ERROR getting data" << endl;
+		else {
+			float result = std::sin(inputData.asFloat());
+			MDataHandle outputHandle = data.outputValue(output);
+			outputHandle.set(result);
+			data.setClean(plug);
+		}
+	}
+
 	return MStatus();
 }
 
 void * cma::KeepDistanceNode::creator(void) {
-	return nullptr;
+	return new KeepDistanceNode;
 }
 
 MStatus cma::KeepDistanceNode::initialize(void) {
-	return MStatus();
+	MFnNumericAttribute nAttr;
+
+	output = nAttr.create("output", "out", MFnNumericData::kFloat, 0.0);
+	nAttr.setWritable(false);
+    nAttr.setStorable(false);
+
+	input = nAttr.create( "input", "in", MFnNumericData::kFloat, 0.0 );
+    nAttr.setStorable(true);
+	
+	addAttribute(input);
+	attributeAffects(input, output);
+	addAttribute(output);
+	
+	return MStatus::kSuccess;
 }
