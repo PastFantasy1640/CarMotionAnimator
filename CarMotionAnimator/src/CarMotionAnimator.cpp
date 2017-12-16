f9e61bb62c5c885fa6a353a16f9ba2d3389ac77c
wgprojects2013 <pastfantasy1640@gmail.com>
2017-12-16 23:07:04 +0900

    プラグインの初期化部分にノード追加用の処理を追加

--- CarMotionAnimator/src/CarMotionAnimator.cpp
+++ CarMotionAnimator/src/CarMotionAnimator.cpp
@@ -20,6 +20,18 @@ private:
 	CommandPair() = delete;
 };
 
+class NodePair {
+public:
+	const MString node;
+	const MTypeId id;
+	void * (*creator)();
+	MStatus (*initialize)();
+	NodePair(const MString & node, const MTypeId id, void * (*creator)(), MStatus (*initialize)()) : node(node), id(id), creator(creator), initialize(initialize) {}
+	virtual ~NodePair() {}
+private:
+	NodePair() = delete;
+};
+
 //*** 新しいコマンドを追加する場所 ***
 std::vector<CommandPair> getCommands(void) {
 	//コマンドをここに追加
@@ -28,16 +40,23 @@ std::vector<CommandPair> getCommands(void) {
 		//CommandPair("Hogehoge", cma::Hogehoge::creator),
 	};
 }
-};
+
+//*** 新しいノードを追加する場所 ***
+std::vector<NodePair> getNodes(void) {
+	//ノードをここに追加
+	//NodePair("ノード名", [Node ID], [クリエイター関数のポインタ], [initialize関数のポインタ]),
+	return std::vector<NodePair> {
+		//NodePair("Hogehoge", cma::Hogehoge::HogehogeID, cma::Hogehoge::creator, cma::Hogehoge::initialize),
+	};
+}
 
 
-/** initializePlugin
- * プラグインの初期化処理を行う。maya側からコールされる関数。
+/** [hidden]addCommands
+ * コマンドを追加する。コマンドの一覧はgetCommands関数内に記述する
+ * @param plugin プラグインインスタンス
+ * @returns 追加に関する結果ステータス
  */
-MStatus cma::initializePlugin(MObject _obj) {
-	MFnPlugin plugin(_obj, "Autodesk", "2015 update2");
-
-	//コマンドの追加
+MStatus addCommands(MFnPlugin & plugin) {
 	std::vector<CommandPair> cmd_pairs = getCommands();
 	MStatus stat;
 	for (auto p = cmd_pairs.begin(); p != cmd_pairs.end(); ++p) {
@@ -48,28 +67,34 @@ MStatus cma::initializePlugin(MObject _obj) {
 			break;
 		}
 	}
-
-	//読み込み完了
-	std::cerr << "Finished to load CarMotionAnimator plug-in." << std::endl;
-	std::cerr << "(c) 2017 S.Shirao  version 0.1" << std::endl;
-
-#ifdef _DEBUG
-	//デバッグ時のみ標準出力をエラー出力へ切り替え
-	std::cout.rdbuf(std::cerr.rdbuf());
-	std::cout << "CarMotionAnimator is debug mode." << std::endl;
-#endif
-
-	return stat;
 }
 
 
-/** uninitializePlugin
- * プラグインのアンロード処理を行う。maya側からコールされる関数。
+/** [hidden]addNodes
+ * ノードを追加する。ノードの一覧はgetNodes関数内に記述する
+ * @param plugin プラグインインスタンス
+ * @returns 追加に関する結果ステータス
  */
-MStatus cma::uninitializePlugin(MObject _obj) {
-	MFnPlugin plugin(_obj);
+MStatus addNodes(MFnPlugin & plugin) {
+	std::vector<NodePair> node_pairs = getNodes();
+	MStatus stat;
+	for (auto p = node_pairs.begin(); p != node_pairs.end(); ++p) {
+		stat = plugin.registerNode(p->node, p->id, p->creator, p->initialize);
+		if (!stat) {
+			//コマンド登録時にエラーが発生
+			stat.perror(std::string("An error occured during registering dipendency node : " + std::string(p->node.asChar())).c_str());
+			break;
+		}
+	}
+}
 
-	//コマンドの登録解除
+
+/** [hidden]removeCommands
+ * コマンドを登録解除する。コマンドの一覧はgetCommands関数内に記述する
+ * @param plugin プラグインインスタンス
+ * @returns 追加に関する結果ステータス
+ */
+MStatus removeCommands(MFnPlugin & plugin) {
 	std::vector<CommandPair> cmd_pairs = getCommands();
 	MStatus stat;
 	for (auto p = cmd_pairs.begin(); p != cmd_pairs.end(); ++p) {
@@ -80,6 +105,72 @@ MStatus cma::uninitializePlugin(MObject _obj) {
 			break;
 		}
 	}
-	
+}
+
+
+/** [hidden]addNodes
+ * ノードを登録解除する。ノードの一覧はgetNodes関数内に記述する
+ * @param plugin プラグインインスタンス
+ * @returns 追加に関する結果ステータス
+ */
+MStatus removeNodes(MFnPlugin & plugin) {
+	std::vector<NodePair> node_pairs = getNodes();
+	MStatus stat;
+	for (auto p = node_pairs.begin(); p != node_pairs.end(); ++p) {
+		stat = plugin.deregisterNode(p->id);
+		if (!stat) {
+			//コマンド登録解除時にエラーが発生
+			stat.perror(std::string("An error occured during deregistering dipendency node : " + std::string(p->node.asChar())).c_str());
+			break;
+		}
+	}
+}
+
+
+
+};
+
+
+/** initializePlugin
+ * プラグインの初期化処理を行う。maya側からコールされる関数。
+ */
+MStatus cma::initializePlugin(MObject _obj) {
+	MFnPlugin plugin(_obj, "Autodesk", "2015 update2");
+
+	MStatus stat = MStatus::kSuccess;
+	do {
+		if ((stat = addCommands(plugin)) != MStatus::kSuccess) break;
+		if ((stat = addNodes(plugin)) != MStatus::kSuccess) break;
+
+	} while (false);
+
+	if (stat == MStatus::kSuccess) {
+		//読み込み完了
+		std::cerr << "Finished to load CarMotionAnimator plug-in." << std::endl;
+		std::cerr << "(c) 2017 S.Shirao  version 0.1" << std::endl;
+#ifdef _DEBUG
+		//デバッグ時のみ標準出力をエラー出力へ切り替え
+		std::cout.rdbuf(std::cerr.rdbuf());
+		std::cout << "CarMotionAnimator is debug mode." << std::endl;
+#endif
+	}
+	else {
+		//読み込み失敗
+		std::cerr << "Failed to initialize plugin." << std::endl;
+	}
+	return stat;
+}
+
+
+/** uninitializePlugin
+ * プラグインのアンロード処理を行う。maya側からコールされる関数。
+ */
+MStatus cma::uninitializePlugin(MObject _obj) {
+	MFnPlugin plugin(_obj);
+	MStatus stat = MStatus::kSuccess;
+	do {
+		if ((stat = removeCommands(plugin)) != MStatus::kSuccess) break;
+		if ((stat = removeNodes(plugin)) != MStatus::kSuccess) break;
+	} while (false);
 	return stat;
 }
